fd90795e4cedc4acef657d8d251431a2679327bb
jatin verma <42065404+jv640@users.noreply.github.com>
2020-08-11 09:32:45 +0530

    Update N meeting in a room.cpp

--- Day 8/N meeting in a room.cpp
+++ Day 8/N meeting in a room.cpp
@@ -1,36 +1,74 @@
-Sort all pairs(Meetings) in increasing order of second number(Finish time) of each pair.
-Select first meeting of sorted pair as the first Meeting in the room and push it into result vector and set a variable time_limet(say) with the second value(Finishing time) of the first selected meeting.
-Iterate from the second pair to last pair of the array and if the value of the first element(Starting time of meeting) of the current pair is greater then previously selected pair finish time (time_limit) then select the current pair and update the result vector (push selected meeting number into vector) and variable time_limit.
-Print the Order of meeting from vector.
-
-
-
-sort(meet, meet + n, comparator); 
-  
-    // Vector for storing selected meeting. 
-    vector<int> m; 
-  
-    // Initially select first meeting. 
-    m.push_back(meet[0].pos); 
-  
-    // time_limit to check whether new  
-    // meeting can be conducted or not. 
-    int time_limit = meet[0].end; 
-  
-    // Check for all meeting whether it  
-    // can be selected or not. 
-    for (int i = 1; i < n; i++) { 
-        if (meet[i].start >= time_limit) 
-        { 
-            // Push selected meeting to vector 
-            m.push_back(meet[i].pos); 
-              
-            // Update time limit. 
-            time_limit = meet[i].end;  
-        } 
-    } 
-  
-    // Print final selected meetings. 
-    for (int i = 0; i < m.size(); i++) { 
-        cout << m[i] << " "; 
-    } 
+/*
+  There is one meeting room in a firm. There are N meetings in the form of (S[i], F[i]) where S[i] is start time of meeting i and F[i] is finish time of meeting i.
+  What is the maximum number of meetings that can be accommodated in the meeting room?
+  Input:  The first line of input consists number of the test cases. The description of T test cases is as follows:
+          The first line consists of the size of the array, second line has the array containing the starting time of all the meetings each separated by a space,
+          i.e., S [ i ]. And the third line has the array containing the finishing time of all the meetings each separated by a space, i.e., F [ i ].
+  Output:
+          In each separate line print the order in which the meetings take place separated by a space.
+  Constraints:
+          1 ≤ T ≤ 70
+          1 ≤ N ≤ 100
+          1 ≤ S[ i ], F[ i ] ≤ 100000
+  Example:
+  Input:
+          2
+          6
+          1 3 0 5 8 5
+          2 4 6 7 9 9
+          8
+          75250 50074 43659 8931 11273 27545 50879 77924
+          112960 114515 81825 93424 54316 35533 73383 160252  
+  Output:
+          1 2 4 5
+          6 7 1
+*/
+#include<bits/stdc++.h>
+using namespace std;
+bool comp(pair<pair<int, int>, int> p1, pair<pair<int, int>, int> p2){
+    return p1.first.second < p2.first.second;
+}
+int main()
+ {
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        vector<int> st;
+        for(int i = 0; i<n; i++){
+            int v;
+            cin>>v;
+            st.push_back(v);
+        }
+        vector<int> ft;
+        for(int i = 0; i<n; i++){
+            int v;
+            cin>>v;
+            ft.push_back(v);
+        }
+        vector<pair<pair<int, int>, int>> meetings;
+        for(int i= 0; i<n; i++){
+            meetings.push_back(make_pair(make_pair(st[i], ft[i]), i+1));
+        }
+        sort(meetings.begin(), meetings.end(), comp);
+        int count = 1;
+        vector<int> allowed;
+        allowed.push_back(meetings[0].second);
+        pair<int, int> curr = meetings[0].first;
+        for(int i = 1; i<n; i++){
+            if(meetings[i].first.first >= curr.second){
+                count++;
+                curr = meetings[i].first;
+                allowed.push_back(meetings[i].second);
+            }
+        }
+        // cout<<count<<endl;
+        for(int i = 0; i<allowed.size(); i++)
+            cout<<allowed[i]<<" ";
+        cout<<endl;
+        // code
+    }
+	
+	return 0;
+}
