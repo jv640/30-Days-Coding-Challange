2d920000ae2f1fd093de3dda190707493488cde6
jatin verma <42065404+jv640@users.noreply.github.com>
2020-09-25 10:42:07 +0530

    Update 4 sum(without Duplicates).cpp

--- Day 4/4 sum(without Duplicates).cpp
+++ Day 4/4 sum(without Duplicates).cpp
@@ -37,3 +37,37 @@ vector<vector<int>> fourSum(vector<int>& nums, int target) {
 /*
   The main catch here is we can calculate k sum by looping on k and make recursive calls to k-1 until k==2 when k = 2 you apply two sum formula/
 */
+
+// code
+vector<vector<int>> fourSum(vector<int>& nums, int target) {
+    sort(begin(nums), end(nums));
+    return kSum(nums, target, 0, 4);
+}
+vector<vector<int>> kSum(vector<int>& nums, int target, int start, int k) {
+    vector<vector<int>> res;
+    if (start == nums.size() || nums[start] * k > target || target > nums.back() * k)
+        return res;
+    if (k == 2)
+        return twoSum(nums, target, start);
+    for (int i = start; i < nums.size(); ++i)
+        if (i == start || nums[i - 1] != nums[i])
+            for (auto &set : kSum(nums, target - nums[i], i + 1, k - 1)) {
+                res.push_back({nums[i]});
+                res.back().insert(end(res.back()), begin(set), end(set));
+            }
+    return res;
+}
+vector<vector<int>> twoSum(vector<int>& nums, int target, int start) {
+    vector<vector<int>> res;
+    int lo = start, hi = nums.size() - 1;
+    while (lo < hi) {
+        int sum = nums[lo] + nums[hi];
+        if (sum < target || (lo > start && nums[lo] == nums[lo - 1]))
+            ++lo;
+        else if (sum > target || (hi < nums.size() - 1 && nums[hi] == nums[hi + 1]))
+            --hi;
+        else
+            res.push_back({ nums[lo++], nums[hi--]});
+    }
+    return res;
+}
