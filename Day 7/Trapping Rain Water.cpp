9b6cf8c2d9a203899081f4ecbdd049cc665256a0
jatin verma <42065404+jv640@users.noreply.github.com>
2020-09-25 12:06:13 +0530

    added two pointer approach

--- Day 7/Trapping Rain Water.cpp
+++ Day 7/Trapping Rain Water.cpp
@@ -41,6 +41,41 @@
         }
         return ans;
     }
+
+// Two pointer approach
+int findWater(int arr[], int n) 
+{ 
+    // initialize output 
+    int result = 0; 
+  
+    // maximum element on left and right 
+    int left_max = 0, right_max = 0; 
+  
+    // indices to traverse the array 
+    int lo = 0, hi = n - 1; 
+  
+    while (lo <= hi) { 
+        if (arr[lo] < arr[hi]) { 
+            if (arr[lo] > left_max) 
+                // update max in left 
+                left_max = arr[lo]; 
+            else
+                // water on curr element = max - curr 
+                result += left_max - arr[lo]; 
+            lo++; 
+        } 
+        else { 
+            if (arr[hi] > right_max) 
+                // update right maximum 
+                right_max = arr[hi]; 
+            else
+                result += right_max - arr[hi]; 
+            hi--; 
+        } 
+    } 
+  
+    return result; 
+} 
     
 // Approach first approach is to find max left height and max right height for every index. then water stored will be min(lmax, rmax) - h[i]
 // but this will take O(n2) as we are finding lmax and rmax for every iteration so O(n2) but if we save lmax and rmax for each i. then we will have the DP approach  
