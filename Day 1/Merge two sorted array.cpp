1907747222bf7635328479b0ec24a91e64e08e4e
jatin verma <42065404+jv640@users.noreply.github.com>
2022-11-28 19:07:07 +0530

    Update Merge two sorted array.cpp

--- Day 1/Merge two sorted array.cpp
+++ Day 1/Merge two sorted array.cpp
@@ -46,7 +46,23 @@ Constraints:
   push all element of arr2 in last of arr1 then compare arr1[i], arr[i+gap] if former is greater than swap otherwise increase i by 1 until i+gap<n;
   then reduce gap to half and perform again eventually array will get sort
   TC =  O((n+m)*log(n+m)) 
+
+One more approach is instead of comparing and sorting from start, start from the end and keep larger number at end in this way it will only take O(m+n)
  
  */
+// Code 
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        int i = m-1, j = n-1, k = m + n - 1;
+        while(i>-1 && j>-1){
+            if(nums1[i] < nums2[j]){
+                nums1[k--] = nums2[j--];
+            }
+            else{
+                nums1[k--] = nums1[i--];
+            }
+        }
+        while(j>-1)
+            nums1[k--] = nums2[j--];
+    }
   
   
